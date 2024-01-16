3b220094fece0f1fb174a50c19aea416dbb7a63f
sophia-mpressed <sophia@mpressed.ai>
2024-01-15 19:45:02 -0600

    Refactor PostsPage component to use Prisma for fetching posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,23 +1,15 @@
 import AddPostForm from "@/components/add-post-form";
 import PostsList from "@/components/posts-list";
+import prisma from "@/lib/db";
 
 export default async function PostsPage() {
-    const randomNumber = Math.floor(Math.random() * 10) + 1;
-    const response = await fetch(
-        `https://dummyjson.com/posts?limit=${randomNumber}`,
-        {
-            next: {
-                revalidate: 3600,
-            },
-        }
-    );
-    const data = await response.json();
+    const posts = await prisma.post.findMany();
 
     return (
         <main className="text-center pt-16 px-5">
-            <AddPostForm />
             <h1 className="text-5xl font-semibold mb-7">All Posts</h1>
-            <PostsList posts={data.posts} />
+            <PostsList posts={posts} />
+            <AddPostForm />
         </main>
     )
 };
